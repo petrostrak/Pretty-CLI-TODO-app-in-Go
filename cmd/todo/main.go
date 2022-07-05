4bd8076be41f37b3813e2c3e686f90e7169051f0
petrostrak <pit.trak@gmail.com>
2022-07-05 13:18:26 +0300

    Test delete functionality

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -15,6 +15,7 @@ const (
 func main() {
 	add := flag.Bool("add", false, "add a new task")
 	complete := flag.Int("complete", 0, "mark a task as completed")
+	del := flag.Int("del", 0, "delete a task")
 
 	flag.Parse()
 
@@ -26,6 +27,7 @@ func main() {
 	}
 
 	switch {
+	//go run ./cmd/todo -add
 	case *add:
 		todos.Add("sample todo")
 		err := todos.Store(TODO_FILE)
@@ -33,8 +35,21 @@ func main() {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
+	//go run ./cmd/todo -complete=1
 	case *complete > 0:
 		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		err = todos.Store(TODO_FILE)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *del > 0:
+		err := todos.Delete(*del)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
