c3345d6302117e1c1598de0f50e725572a874cfe
petrostrak <pit.trak@gmail.com>
2022-07-05 13:08:33 +0300

    Test add functionality

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	todo "github.com/petrostrak/Pretty-CLI-TODO-app-in-Go"
+)
+
+const (
+	TODO_FILE = ".todos.json"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	add := flag.Bool("add", false, "add a new todo")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(TODO_FILE); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("sample todo")
+		err := todos.Store(TODO_FILE)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
 }
